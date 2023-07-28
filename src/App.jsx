db41c7019f2a11e5005e8d5fa0d18f98f17ffd4e
KordyD <84672403+KordyD@users.noreply.github.com>
2023-07-28 19:49:57 +0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import Advertising from './modules/Advertising/Advertising';
 import Boost from './modules/Boost/Boost';
+import Footer from './modules/Footer/Footer';
 import Header from './modules/Header/Header';
 import Hero from './modules/Hero/Hero';
 import Shortening from './modules/Shortening/Shortening';
@@ -17,49 +18,8 @@ function App() {
       <Advertising />
 
       <Boost />
-      <footer>
-        <div>
-          <h3>Features</h3>
-          <ul>
-            <li>Link Shortening</li>
-            <li>Branded Links</li>
-            <li>Analytics</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Resources</h3>
-          <ul>
-            <li>Blog</li>
-            <li>Developers</li>
-            <li>Support</li>
-          </ul>
-        </div>
-        <div>
-          <h3>Company</h3>
-          <ul>
-            <li>About</li>
-            <li>Our Team</li>
-            <li>Careers</li>
-            <li>Contact</li>
-          </ul>
-        </div>
 
-        <div>
-          <a href='#' className='logo-fb' role='img' aria-label='facebook'>
-            <img src='/images/icon-facebook.svg' />
-          </a>
-          <a href='#' className='logo-tw' role='img' aria-label='twitter'>
-            <img src='/images/icon-twitter.svg' />
-          </a>
-          <a href='#' className='logo-pt' role='img' aria-label='pinterest'>
-            <img src='/images/icon-pinterest.svg' />
-          </a>
-          <a href='#' className='logo-inst' role='img' aria-label='instagram'>
-            <img src='/images/icon-instagram.svg' />
-          </a>
-        </div>
-      </footer>
-
+      <Footer />
       {/* <div className='attribution'>
         Challenge by{' '}
         <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
---
+++ src/modules/Footer/Footer.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+const Footer = () => {
+  return (
+    <div className={styles.footerBackground}>
+      <footer className={`${styles.footer} container`}>
+        <a href='' role='img' aria-label='shortly'>
+          <img alt='Shortly logo' src='/src/assets/logo-white.svg' />
+        </a>
+
+        <div className={styles.linksWrapper}>
+          <div>
+            <h3>Features</h3>
+            <ul>
+              <li>
+                <a className='link footer__link' href=''>
+                  Link Shortening
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Branded Links
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Analytics
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Resources</h3>
+            <ul>
+              <li>
+                <a className='link footer__link' href=''>
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Developers
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Support
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Company</h3>
+            <ul>
+              <li>
+                <a className='link footer__link' href=''>
+                  About
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Our Team
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Careers
+                </a>
+              </li>
+              <li>
+                <a className='link footer__link' href=''>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={styles.iconWrapper}>
+          <a
+            href=''
+            className={`${styles.facebook} link footer__icon__link`}
+            role='img'
+            aria-label='facebook'
+          ></a>
+          <a
+            href=''
+            className={`${styles.twitter} link footer__icon__link`}
+            role='img'
+            aria-label='twitter'
+          ></a>
+          <a
+            href=''
+            className={`${styles.pinterest} link footer__icon__link`}
+            role='img'
+            aria-label='pinterest'
+          ></a>
+          <a
+            href=''
+            className={`${styles.instagram} link footer__icon__link`}
+            role='img'
+            aria-label='instagram'
+          ></a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
