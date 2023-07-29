379159e3b724a668beb84cbd91d5ab38c2eb59e9
KordyD <84672403+KordyD@users.noreply.github.com>
2023-07-29 14:23:58 +0300

    Add adaptive styling

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const [state, setState] = useState('');
+
   return (
     <header className={`${styles.header} container`}>
-      <div className={styles.navigationWrapper}>
-        <a href='' role='img' aria-label='shortly'>
-          <img alt='Shortly logo' src='/src/assets/logo.svg' />
-        </a>
+      <a href='' role='img' aria-label='shortly'>
+        <img alt='Shortly logo' src='/src/assets/logo.svg' />
+      </a>
+      <div className={styles.navigation}>
         <nav>
           <ul>
             <li>
@@ -27,11 +29,51 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+
+        <div className={styles.buttons}>
+          <button className='button transparent'>Login</button>
+          <button className='button basic'>Sign Up</button>
+        </div>
       </div>
 
-      <div className={styles.buttons}>
-        <button className='button transparent'>Login</button>
-        <button className='button basic'>Sign Up</button>
+      <button
+        onClick={() => setState((state) => (state ? '' : 'active'))}
+        className={styles.burgerButton}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      <div
+        className={`${styles.navigationBurger} ${
+          state ? styles['active'] : styles['inActive']
+        }`}
+      >
+        <nav>
+          <ul>
+            <li>
+              <a className='link burger__link ' href=''>
+                Features
+              </a>
+            </li>
+            <li>
+              <a className='link burger__link' href=''>
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a className='link burger__link' href=''>
+                Resources
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.buttons}>
+          <button className='button transparent burger__button'>Login</button>
+          <button className='button basic'>Sign Up</button>
+        </div>
       </div>
     </header>
   );
